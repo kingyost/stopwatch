b16ea538c105a592b7b249ac794be1688d93272c
KingPin <KingPin@EmberCelica>
2019-08-04 15:44:49 -0400

    comment

---
+++ CountUpTimer/src/stopwatch/UuuggghhhLearning.java
@@ -0,0 +1,32 @@
+package stopwatch;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class UuuggghhhLearning {
+	
+	Timer timer;
+	
+	public UuuggghhhLearning(int sec){
+		
+	timer = new Timer();
+	timer.schedule(new Goodbye(), sec * 1000);
+	}
+	
+	class Goodbye extends TimerTask{
+		public void run() {
+			System.out.println("Goodbye!");
+			timer.cancel();
+		}
+	}
+
+
+
+	public static void main(String[] args) {
+		
+		new UuuggghhhLearning(5);
+		System.out.println("START!!");
+		
+	}
+
+}
